19ead35dce296eed9af4203de9cbc0ee84bcc3bb
nirmeshjg19 <47989444+nirmeshjg19@users.noreply.github.com>
2020-04-15 17:09:49 +0530

    ..

--- RDS(postgres)/autoscaling.tf
+++ RDS(postgres)/autoscaling.tf
@@ -49,20 +49,3 @@ resource "aws_autoscaling_group" "app-autoscaling" {
 
 
 
-// resource "aws_instance" "instance" {
-//   ami           = "ami-0c28d7c6dd94fb3a7"
-//   instance_type = "t2.micro"
-
-//   # the VPC subnet
-//   subnet_id = aws_subnet.app-vpc-public.id
-
-//   # the security group
-//   vpc_security_group_ids = [aws_security_group.sg-instance.id]
-
-//   # the public SSH key
-//   key_name = aws_key_pair.deployer.key_name
-
-//   tags ={
-//     Name="instancePost"
-//   }
-// }
